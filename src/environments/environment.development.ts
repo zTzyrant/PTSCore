70369571dd707ddb898438613689da60ab2996c5
zTzyrant <61049884+zTzyrant@users.noreply.github.com>
2023-11-23 21:22:42 +0800

    test: for testing env use env.dev for access backend via dev tunnel

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "http://192.168.18.14:3000",
+  apiUrl: "https://q06n1njk-3000.asse.devtunnels.ms",
+  // apiUrl: "http://192.168.18.14:3000",
 }
