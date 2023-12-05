6280fe7e1000e29d16f15d67ee6f88832d7dcddf
zTzyrant <61049884+zTzyrant@users.noreply.github.com>
2023-12-06 05:08:31 +0800

    fix: scroll position restoration option in app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -164,9 +164,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { scrollPositionRestoration: "disabled" }),
-  ],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: "top" })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
