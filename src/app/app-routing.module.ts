57636358076cd2217123388fd8a5ee0c6e65f747
zTzyrant <61049884+zTzyrant@users.noreply.github.com>
2023-12-06 05:02:53 +0800

    fix: Update scroll position restoration now spiner loding will always on centered page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -164,7 +164,9 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: "top" })],
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: "disabled" }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
