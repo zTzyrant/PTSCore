198df04bd5d8d31aaa99f80b9bd6bdbbdbcfe13f
zTzyrant <61049884+zTzyrant@users.noreply.github.com>
2023-12-08 05:10:00 +0800

    feat: Add assets, build configuration, and access control files for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://localhost:3000", // your prod server
+  apiUrl: "https://api.takidev.my.id", // your prod server
   // apiUrl: "http://192.168.18.14:3000",
 }
